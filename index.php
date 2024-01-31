cb58ee434fb1162f78512b877c47f39a75b0afff
AKraja777 <ashokkumar7hitter@gmail.com>
2024-01-31 15:35:17 +0530

    index.php

--- index.php
+++ index.php
@@ -304,7 +304,8 @@ input[type="number"] {
                 </div>
     <div class="col-lg-6 col-md-6 col-12">
         <h1 style="color:black; font-size: 2.5em;" data-aos="fade-up">APPLY NOW</h1>
-        <form method="post" action="index.php" enctype="multipart/form-data" data-aos="fade-up">
+        <form method="post" action="#" enctype="multipart/form-data" data-aos="fade-up">
+        <input type="hidden" name="refer_code" value="<?php echo $refer_code; ?>">
     <input type="text"  class="form-control" id="name" name="name" placeholder="enter your name" required>
     <input type="mail"  class="form-control" id="email" name="email" placeholder="enter your mail" name="email" required>
     <input type="number"  class="form-control" id="mobile" name="mobile" placeholder="enter your Contact Number" name="mobile" required>
@@ -385,8 +386,6 @@ function togglePanel(panelHeader) {
         }
     }
 </script>
-
-
     <!-- Bootstrap JS and Popper.js -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
    
